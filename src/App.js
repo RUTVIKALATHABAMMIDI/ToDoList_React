21cac2d0b2daae7327ec5d8f0eadab1e2acada12
RUTVIKA LATHA BAMMIDI <91607274+RUTVIKALATHABAMMIDI@users.noreply.github.com>
2021-12-08 01:00:31 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Inside from "./components/inside.js";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="Main_outer">ToDo List</div>
+      <Inside />
+    </div>
+  );
+}
+
